7eb8a87efd7259ee1867e68bf767e9dad7ab6c39
Tal Zigelnik <zigelnik@gmail.com>
2026-05-11 10:47:23 +0300

    created tests for simple app with pytest

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({
-        "message": "Hello from Flask!",
-        "status": "ok"
+        "message": "CI/CD Demo."
     })
 
-@app.route("/tal", methods=["GET"])
+@app.route("/health", methods=["GET"])
 def health():
+    return jsonify({
+        "status": "ok"
+    })
+@app.route("/tal", methods=["GET"])
+def tal():
     return "working", 200
 
 
@@ -24,4 +28,4 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
